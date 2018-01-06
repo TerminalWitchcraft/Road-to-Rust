5a9de2bc41c0ece70aa86ab62865764fb3c8c6e8
Hitesh Paul <hiteshpaul94@gmail.com>
2018-01-06 21:45:17 +0530

    Completed minigrep

---
+++ book_2nd_edition/minigrep/src/main.rs
@@ -0,0 +1,29 @@
+extern crate minigrep;
+
+use std::env;
+use std::process;
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    //let query = &args[1];
+    //let filename = &args[2];
+    let config = Config::new(&args)
+        .unwrap_or_else(|err| {
+            eprintln!("Error parsing arguements: {}", err);
+            process::exit(1);
+        });
+    println!("The args is :: {:?}", args);
+    println!("Searching for {} in file {}", config.query, config.filename);
+
+    //Open the file
+    //let mut f = File::open(config.filename).expect("File not found!!!");
+    //let mut contents = String::new();
+    //f.read_to_string(&mut contents).expect("Failed to read contents");
+    //println!("The contents of the file is:\n ==========\n {}", contents);
+    //run(config);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
