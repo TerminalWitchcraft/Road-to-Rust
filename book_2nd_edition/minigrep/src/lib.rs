f83c88ca80d8d6f2897b4592f9b320d4f10bf0b5
Hitesh Paul <hiteshpaul94@gmail.com>
2018-01-10 23:52:37 +0530

    Remove clone and use itertor

--- book_2nd_edition/minigrep/src/lib.rs
+++ book_2nd_edition/minigrep/src/lib.rs
@@ -27,13 +27,22 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            //panic!("Too few arguements, provide at least 2!")
-            return Err("Too few arguements, provide at least 2!");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        //if args.len() < 3 {
+        //    //panic!("Too few arguements, provide at least 2!")
+        //    return Err("Too few arguements, provide at least 2!");
+        //}
+        //let query = args[1].clone();
+        //let filename = args[2].clone();
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("didn't get a filename string"),
+        };
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         Ok(Config { query, filename, case_sensitive })
     }
@@ -42,27 +51,33 @@ impl Config {
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
+    //let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
+    //for line in contents.lines() {
+    //    if line.to_lowercase().contains(&query) {
+    //        results.push(line);
+    //    }
+    //}
 
-    results
+    //results
+    contents.lines()
+        .filter(|x| x.contains(&query))
+        .collect()
 }
 
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     //vec![]
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    //let mut results = Vec::new();
+    //for line in contents.lines() {
+    //    if line.contains(query) {
+    //        results.push(line);
+    //    }
+    //}
+    //results
+    contents.lines()
+        .filter(|x| x.contains(query))
+        .collect()
 }
 
 #[cfg(test)]
--- book_2nd_edition/minigrep/src/main.rs
+++ book_2nd_edition/minigrep/src/main.rs
@@ -5,15 +5,16 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // Change in ch13
+    //let args: Vec<String> = env::args().collect();
     //let query = &args[1];
     //let filename = &args[2];
-    let config = Config::new(&args)
+    //let config = Config::new(&args)
+    let config = Config::new(env::args())
         .unwrap_or_else(|err| {
             eprintln!("Error parsing arguements: {}", err);
             process::exit(1);
         });
-    println!("The args is :: {:?}", args);
     println!("Searching for {} in file {}", config.query, config.filename);
 
     //Open the file
