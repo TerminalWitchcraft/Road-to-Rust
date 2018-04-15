fa14b182d20beedf474ed863fa8f26c1db36db4d
Hitesh Paul <hiteshpaul94@gmail.com>
2018-04-15 17:25:00 +0530

    Added code for ref str

--- book_2nd_edition/ch15/main.rs
+++ book_2nd_edition/ch15/main.rs
@@ -17,10 +17,17 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
+fn hello(x: &str) {
+    println!("Hello, {}", x);
+}
+
 fn main() {
     let x = 5;
     let y = MyBox::new(5);
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Hitesh"));
+    hello(&m);
 }
